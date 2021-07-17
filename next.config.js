73f555450f13efebae41f26c582e2e902d86b9b6
maxwowo <maxwo@protonmail.com>
2021-07-17 15:00:53 +1200

    Set up Chakra UI and images API rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: '/api/images/:path*',
+      destination: 'https://files.maxwowo.com/file/mwo-photos/:path*',
+    },
+  ],
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+)
+
+export default App
