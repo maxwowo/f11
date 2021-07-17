87dec2e8eb4092b165f64362c53928a642a6ab56
maxwowo <maxwo@protonmail.com>
2021-07-17 16:00:24 +1200

    Create masonry component

--- src/components/Masonry.tsx
+++ src/components/Masonry.tsx
@@ -1,3 +1,43 @@
-const Masonry = () => <div>masonry</div>
+import { Box, BoxProps } from '@chakra-ui/react'
+import Image from 'next/image'
+import { FunctionComponent, memo } from 'react'
 
-export default Masonry
+export interface MasonryProps {
+  images: {
+    filename: string
+    description: string
+  }[]
+}
+
+const Masonry: FunctionComponent<MasonryProps & BoxProps> = ({
+  images,
+  ...rest
+}) => (
+  <Box
+    {...rest}
+    sx={{
+      columnCount: [1, 2, 3, 4],
+      columnGap: 20,
+    }}
+  >
+    {images.map((image) => (
+      <Box
+        key={image.filename}
+        display="inline-block"
+        marginBottom={20}
+        marginLeft={0}
+        marginRight={20}
+        marginTop={0}
+        width="100%"
+      >
+        <Image
+          alt={image.description}
+          layout="fill"
+          src={`/api/images/${image.filename}`}
+        />
+      </Box>
+    ))}
+  </Box>
+)
+
+export default memo(Masonry)
