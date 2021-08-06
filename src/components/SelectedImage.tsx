be132c1236c860f11c55af9a79df872dace24819
maxwowo <maxwo@protonmail.com>
2021-08-06 22:15:03 +1200

    Set minimum loading time

--- src/components/SelectedImage.tsx
+++ src/components/SelectedImage.tsx
@@ -1,7 +1,9 @@
 import { Box, Image as ChakraImage, Spinner } from '@chakra-ui/react'
-import { Fragment, FunctionComponent, memo, useState } from 'react'
+import { Fragment, FunctionComponent, memo, useEffect, useState } from 'react'
 
 import { Image } from '../images'
+
+const MINIMUM_LOADING_TIME = 300
 
 interface SelectedImageProps {
   selectedImage: Image | null
@@ -11,6 +13,18 @@ const SelectedImage: FunctionComponent<SelectedImageProps> = ({
   selectedImage,
 }) => {
   const [loading, setLoading] = useState(true)
+  const [waiting, setWaiting] = useState(true)
+  const visible = !loading && !waiting
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWaiting(false)
+    }, MINIMUM_LOADING_TIME)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   const handleImageLoad = () => {
     setLoading(false)
@@ -18,14 +32,14 @@ const SelectedImage: FunctionComponent<SelectedImageProps> = ({
 
   return (
     <Fragment>
-      {loading && (
+      {!visible && (
         <Box padding={[25, 50]}>
           <Spinner size="lg" />
         </Box>
       )}
       <ChakraImage
         alt={selectedImage?.description}
-        display={loading ? 'none' : undefined}
+        display={visible ? undefined : 'none'}
         maxHeight="85vh"
         src={selectedImage?.filename}
         onError={handleImageLoad}
