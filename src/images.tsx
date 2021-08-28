f3c0e406d71d926900e2bf5bf7bea76e7adbd971
maxwowo <maxwo@protonmail.com>
2021-08-29 00:42:47 +1200

    Remove pictures I don't like

--- src/images.tsx
+++ src/images.tsx
@@ -14,6 +14,12 @@ export interface Image {
 
 const images: Image[] = [
   {
+    filename: '2021_08_18_sky_tower_new_world_cbd',
+    tags: [Tag.URBAN],
+    width: 3485,
+    height: 2323,
+  },
+  {
     filename: '2021_08_18_beach_takapuna',
     tags: [Tag.URBAN],
     width: 2425,
@@ -24,24 +30,6 @@ const images: Image[] = [
     tags: [Tag.URBAN],
     width: 2425,
     height: 3637,
-  },
-  {
-    filename: '2021_08_18_petrol_station_pokeno',
-    tags: [Tag.URBAN],
-    width: 2359,
-    height: 3538,
-  },
-  {
-    filename: '2021_08_18_sky_tower_cbd',
-    tags: [Tag.URBAN],
-    width: 2353,
-    height: 3530,
-  },
-  {
-    filename: '2021_08_18_sky_tower_new_world_cbd',
-    tags: [Tag.URBAN],
-    width: 3485,
-    height: 2323,
   },
 ]
 
