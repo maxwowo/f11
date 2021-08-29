6c2571be4411190ce43c75ec22058c291d5afd14
maxwowo <maxwo@protonmail.com>
2021-08-29 22:31:07 +1200

    Remove unnecessary animation

--- src/components/MasonryImage.tsx
+++ src/components/MasonryImage.tsx
@@ -1,8 +1,6 @@
 import { Image as ChakraImage } from '@chakra-ui/image'
 import { Box } from '@chakra-ui/layout'
-import { motion, useAnimation } from 'framer-motion'
-import { FunctionComponent, memo, useEffect } from 'react'
-import { useInView } from 'react-intersection-observer'
+import { FunctionComponent, memo } from 'react'
 
 import { Image } from '../images'
 
@@ -13,41 +11,15 @@ export interface MasonryImageProps {
   }
 }
 
-const MasonryImage: FunctionComponent<MasonryImageProps> = ({ data }) => {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-
-  useEffect(() => {
-    if (inView) {
-      controls.start('visible')
-    }
-  }, [controls, inView])
-
-  return (
-    <motion.div
-      ref={ref}
-      animate={controls}
-      initial="hidden"
-      transition={{
-        duration: 0.5,
-      }}
-      variants={{
-        visible: { opacity: 1, y: 0 },
-        hidden: { opacity: 0, y: 20 },
-      }}
-    >
-      <Box
-        _hover={{ opacity: '0.8' }}
-        cursor="pointer"
-        transition="opacity 0.2s ease"
-        onClick={() => data.handleImageSelect(data.image)}
-      >
-        <ChakraImage
-          src={`/api/images/${data.image.filename}.thumbnail.webp`}
-        />
-      </Box>
-    </motion.div>
-  )
-}
+const MasonryImage: FunctionComponent<MasonryImageProps> = ({ data }) => (
+  <Box
+    _hover={{ opacity: '0.8' }}
+    cursor="pointer"
+    transition="opacity 0.2s ease"
+    onClick={() => data.handleImageSelect(data.image)}
+  >
+    <ChakraImage src={`/api/images/${data.image.filename}.thumbnail.webp`} />
+  </Box>
+)
 
 export default memo(MasonryImage)
