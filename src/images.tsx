dc27de46f9ff2edb081ae1670c2fbbe593d38a27
maxwowo <maxwo@protonmail.com>
2021-08-29 16:20:13 +1200

    Add more images

--- src/images.tsx
+++ src/images.tsx
@@ -12,6 +12,26 @@ export interface Image {
 
 const images: Image[] = [
   {
+    filename: 'a741b1acd4f74ec5b8807030750ec8a9',
+    tags: [Tag.LANDSCAPE],
+  },
+  {
+    filename: '62e3b837e850437eb404797a506dd08d',
+    tags: [Tag.LANDSCAPE],
+  },
+  {
+    filename: '47e73121778846359ec02ab5bdc0bc58',
+    tags: [Tag.LANDSCAPE],
+  },
+  {
+    filename: '6f67d7df80a34dcfa5694a6d822f649c',
+    tags: [Tag.LANDSCAPE],
+  },
+  {
+    filename: '0d135737424f48cb91b1cf7a7b5eac1e',
+    tags: [Tag.LANDSCAPE],
+  },
+  {
     filename: '27e302fd627b456aa775f5127152ba22',
     tags: [Tag.URBAN],
   },
