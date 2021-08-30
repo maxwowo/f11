1a2956d27f3a2d27e23cb96b603127b6a2d2eb0e
maxwowo <maxwo@protonmail.com>
2021-08-30 16:44:45 +1200

    Add skeleton to masonry

--- src/components/MasonryImage.tsx
+++ src/components/MasonryImage.tsx
@@ -1,8 +1,8 @@
 import { Image as ChakraImage } from '@chakra-ui/image'
 import { Box } from '@chakra-ui/layout'
-import { Link } from '@chakra-ui/react'
+import { Link, Skeleton } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import { FunctionComponent, memo } from 'react'
+import { FunctionComponent, memo, useState } from 'react'
 
 import { Image } from '../images'
 
@@ -12,17 +12,25 @@ export interface MasonryImageProps {
 
 const MasonryImage: FunctionComponent<MasonryImageProps> = ({
   data: image,
-}) => (
-  <Box
-    _hover={{ opacity: '0.8' }}
-    cursor="pointer"
-    transition="opacity 0.2s ease"
-    onClick={() => console.log(`${image.filename} clicked`)}
-  >
-    <Link as={NextLink} href={`/images/${image.filename}`}>
-      <ChakraImage src={`/api/images/${image.filename}.thumbnail.webp`} />
-    </Link>
-  </Box>
-)
+}) => {
+  const [isLoaded, setIsLoaded] = useState(false)
 
+  return (
+    <Skeleton isLoaded={isLoaded}>
+      <Box
+        _hover={{ opacity: '0.8' }}
+        cursor="pointer"
+        transition="opacity 0.2s ease"
+        onClick={() => console.log(`${image.filename} clicked`)}
+      >
+        <Link as={NextLink} href={`/images/${image.filename}`}>
+          <ChakraImage
+            src={`/api/images/${image.filename}.thumbnail.webp`}
+            onLoad={() => setIsLoaded(true)}
+          />
+        </Link>
+      </Box>
+    </Skeleton>
+  )
+}
 export default memo(MasonryImage)
